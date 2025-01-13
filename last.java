7194a9b3d955742641f10849b16c73a41f7215f5
AKASH T A <149130040+TaAkash2022004@users.noreply.github.com>
2025-01-13 20:36:23 +0530

    Add files via upload

---
+++ last.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class last{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n;
+        System.out.println("Enter a no:");
+        n=s.nextInt();
+
+        System.out.println("Last digit is:"+(n%10));
+
+
+    }
+    }
